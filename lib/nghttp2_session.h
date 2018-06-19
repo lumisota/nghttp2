2ebad6fc1548fd3c7ab548e7213726136925ce6a
Stephen McQuistin <sm@smcquistin.uk>
2018-06-19 14:35:26 +0100

    Update nghttp2_session.h

--- lib/nghttp2_session.h
+++ lib/nghttp2_session.h
@@ -79,7 +79,7 @@ typedef struct {
 
 /* Buffer length for inbound raw byte stream used in
    nghttp2_session_recv(). */
-#define NGHTTP2_INBOUND_BUFFER_LENGTH 16384
+#define NGHTTP2_INBOUND_BUFFER_LENGTH 16386
 
 /* The default maximum number of incoming reserved streams */
 #define NGHTTP2_MAX_INCOMING_RESERVED_STREAMS 200
